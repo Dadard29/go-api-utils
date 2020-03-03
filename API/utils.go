f470317776d88c9a340e589b7b2569e96f183a9c
florian.charpentier <florian.charpentier@epita.fr>
2020-03-03 10:15:27 +0100

    fixed content type for json response

--- API/utils.go
+++ API/utils.go
@@ -13,7 +13,7 @@ const (
 )
 
 func BuildJsonResponse(status bool, message string, content interface{}, w http.ResponseWriter) error {
-	// w.Header().Add("Content-Type", "application/json")
+	w.Header().Add("Content-Type", "application/json")
 	w.Header().Add("Access-Control-Allow-Origin", "*")
 	w.WriteHeader(http.StatusOK)
 	return json.NewEncoder(w).Encode(response{
